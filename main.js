7b32247c6460a0ec22b373140ffcc1d6a8c62c5a
Phat <02ntphat@gmail.com>
2025-03-19 08:47:09 +0700

    create button and functions

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("1");
+const form = document.querySelector("#form2");
+const btn = document.querySelector("#btn");
+form.onsubmit = (e) => {
+  e.preventDefault();
+};
+
+btn.onclick = () => {
+  alert("Hello");
+};
