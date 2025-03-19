547d571679ffdf58b63ab803ec283c8d1e3dc19e
Phat <02ntphat@gmail.com>
2025-03-19 10:02:02 +0700

    Revert "create button and functions"
    This reverts commit 7b32247c6460a0ec22b373140ffcc1d6a8c62c5a.

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-const form = document.querySelector("#form2");
-const btn = document.querySelector("#btn");
-form.onsubmit = (e) => {
-  e.preventDefault();
-};
-
-btn.onclick = () => {
-  alert("Hello");
-};
+console.log("1");
